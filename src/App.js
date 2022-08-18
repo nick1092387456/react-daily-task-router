db7aceec2c4f6b2e400bb15207b40cf0fc427747
Bumble <nick1092387456@gmail.com>
2022-08-18 18:52:00 +0800

    add: navigate

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import {
   HashRouter,
   NavLink,
   Route,
-  Routes
+  Routes,
+  useNavigate
 } from 'react-router-dom';
 
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return (
+    <>
+      <p>這是 Todo 頁面 </p>
+      <Logout />
+    </>
+  )
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
@@ -16,6 +21,12 @@ const Login = () => {
 const Register = () => {
   return <p>這是註冊頁面</p>;
 };
+const Logout = () => {
+  let navigate = useNavigate()
+  return <>
+  <button onClick={()=>{navigate('/login')}}>登出</button>
+  </> 
+}
 
 function App() {
   return (
